5b239734b03a701884067737f5a60b7c2a98e065
silvermistshadow <bmead72@gmail.com>
2020-02-19 12:59:04 -0800

    write first test

--- CalendarTests/ModelsTests/LeapYearTests.cs
+++ CalendarTests/ModelsTests/LeapYearTests.cs
@@ -9,7 +9,8 @@ namespace CalendarTests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      //testing code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
   }
 }
