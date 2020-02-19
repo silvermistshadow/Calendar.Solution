4c5a197907b8a048133d043ea90354be1cdef701
silvermistshadow <bmead72@gmail.com>
2020-02-19 11:09:58 -0800

    add test files

---
+++ CalendarTests/ModelsTests/LeapYearTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace CalendarTests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      //testing code will go here
+    }
+  }
+}
